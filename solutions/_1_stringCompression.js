e9302c9d4d19ef64ab789a7c4a19fcef946ab411
Ajmain-Fayek <ajmainfayek733@gmail.com>
2026-01-20 00:29:51 +0600

    solve of problem set 1

--- solutions/_1_stringCompression.js
+++ solutions/_1_stringCompression.js
@@ -1,31 +1,42 @@
 /**
  * Write a function that takes a string and returns a **_compressed_** version.
  * If a character repeats, replace the repetition with the count of characters.
- * 
+ *
  * Input: aaabbcbba
  * output: a3b2c1b2a1
  * Goal: This tests your abitlity to handle loops and conditional logic within a single traversal.
  */
 
-const string = "aaabbcbba";
-const stringArray = string.split("");
-const compressedArray = [];
+const stringCompressor = (s) => {
+  if (typeof s === "string") {
+    const stringArray = s.split("");
+    const compressedStringArray = [];
 
-for (let index = 0; index < stringArray.length; index++) {
-const element = stringArray[index];
+    for (let index = 0; index < stringArray.length; index++) {
+      const element = stringArray[index];
 
-  let elementCount = 1;
+      let elementCount = 1;
 
-  for (let idx = index + 1; idx < stringArray.length; idx++) {
-    if (element !== stringArray[idx]) {
-      index = idx - 1;
-      break;
+      for (let idx = index + 1; idx < stringArray.length; idx++) {
+        if (idx === stringArray.length - 1) {
+          index = idx; // to close the parent loop
+        }
+
+        if (element !== stringArray[idx]) {
+          index = idx - 1;
+          break;
+        }
+
+        elementCount++;
+      }
+
+      compressedStringArray.push(element + elementCount);
     }
 
-    elementCount++;
+    return compressedStringArray.join("");
   }
 
-  compressedArray.push(element + elementCount);
-}
+  return "[error] invalide argument type!";
+};
 
-console.log(compressedArray.join(""));
+console.log(stringCompressor("aaabbcbba"));
